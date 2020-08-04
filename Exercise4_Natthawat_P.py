4d364dd5383f2a49c46aba2df3d61424142b9d8c
natthawat3 <68450946+natthawat3@users.noreply.github.com>
2020-08-04 14:23:34 +0700

    Add files via upload

---
+++ Exercise4_Natthawat_P.py
@@ -0,0 +1,13 @@
+FE = 87.6
+GB = 63.2
+ICS = 70.0
+CP = 92.5
+
+print("--- Your Score ---")
+
+print("Foundation English               :", FE)
+print("General Business                 :",GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming             :", CP)
+print("--- Have a nice day ---")
+
