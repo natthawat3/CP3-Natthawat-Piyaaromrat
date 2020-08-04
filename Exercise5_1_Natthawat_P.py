eddeaea7d402ed8362d384e713fa2f6dbb639d59
natthawat3 <68450946+natthawat3@users.noreply.github.com>
2020-08-04 17:34:57 +0700

    Add files via upload

---
+++ Exercise5_1_Natthawat_P.py
@@ -0,0 +1,11 @@
+a = int(input("What number do you like?"))
+b = int(input("What's the date of your birth day?"))
+add = a+b
+subtract = a-b
+multiply = a*b
+divide = a/b
+print("I will show you the calculation.")
+print(a,"+",b,"=",add)
+print(a,"-",b,"=",subtract)
+print(a,"*",b,"=",multiply)
+print(a,"/",b,"=",divide)
---
+++ Exercise5_2_Natthawat_P.py
@@ -0,0 +1,4 @@
+s = float(input("How far do you go?(Metres)"))
+t = float(input("How much time do you spend?(Minutes)"))
+v=(s/1000)/(t/60)
+print("Velocity",v,"km/h")
