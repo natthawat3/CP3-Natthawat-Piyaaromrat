1e6d43c437c0aa7cce0a186e9d99e878dc168d51
natthawat3 <68450946+natthawat3@users.noreply.github.com>
2020-08-06 16:44:56 +0700

    Add files via upload

---
+++ Lecture50_Natthawat_P.py
@@ -0,0 +1,15 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(5,10)
+
+def subtractNumber(x,y):
+    print(x-y)
+subtractNumber(5,1)
+
+def multiplyNumber(x,y):
+    print(x*y)
+multiplyNumber(5,2)
+
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(2,3)
